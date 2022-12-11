7d098befdbcba36794bd7822cd7d69d96d2c66fe
Clustern <homerun372@gmail.com>
2022-12-11 15:11:59 +0900

    첫시간에 작성한 그래프를 파이썬코드로 구현

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 #데이터 불러오기
 filePath = 'A회사_보습제_매출데이터_v1.csv'
 
@@ -9,20 +8,56 @@ salesData = pd.read_csv(filePath, encoding = 'cp949')
 salesData = salesData.transpose()
 salesData.rename(columns = salesData.iloc[0], inplace = True)
 salesData = salesData.drop(salesData.index[0])
+
 salesData
 
-#데이터 전처리
 import re
 
+# 특수기호 제거
 for col in list(salesData.columns):
-    repl = []
+    refineded = []
     for x in list(salesData[col]):
         try:
-            repl.append(int(re.sub(r"[^0-9.]", "", x)))
+            refineded.append(int(re.sub(r"[^0-9.]", "", x)))
         except:
-            repl.append(float(re.sub(r"[^0-9.]", "", x)))
-    salesData[col] = repl
+            refineded.append(float(re.sub(r"[^0-9.]", "", x)))
+    salesData[col] = refineded
     
 salesData
 
+import matplotlib.pyplot as plt
 
+# 이전 과제 
+
+totalSales = list(salesData['총매출'])
+unitPrices = list(salesData['1온스별단가'])
+salesVolumes = []
+
+for idx in range(len(totalSales)):
+    volume = round(totalSales[idx] / unitPrices[idx])
+    salesVolumes.append(volume)
+    
+advPrice = list(salesData['광고비용'])
+months = list(salesData.index)
+
+plt.style.use('default')
+plt.rcParams['font.size'] = 12
+
+fig, ax1 = plt.subplots()
+ax1.plot(months, salesVolumes, color='green')
+ax1.set_ylabel('salesVolumes')
+ax1.grid(True, axis = 'y')
+
+plt.ylim(2400000, 3200000)
+current_values = plt.gca().get_yticks()
+plt.gca().set_yticklabels(['$' + '{:,.0f}'.format(x) for x in current_values])
+
+ax2 = ax1.twinx()
+ax2.set_ylabel('advPrice')
+ax2.plot(months, advPrice, color='deeppink')
+
+plt.ylim(0, 1200000)
+current_values = plt.gca().get_yticks()
+plt.gca().set_yticklabels(['$' + '{:,.0f}'.format(x) for x in current_values])
+
+plt.show()
