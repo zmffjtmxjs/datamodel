894104b5c0d4413b9eede2a07d977dd0ea4a529c
Clustern <homerun372@gmail.com>
2022-12-13 22:56:47 +0900

    사용하지 않는 내용에 대한 코드 제거

--- data_analysis.py
+++ data_analysis.py
@@ -29,52 +29,7 @@ def refined_data(df):
     return df
 
 #=============================================================================================
-
-# 데이터 불러오기
-salesData = load_data('A회사_보습제_매출데이터_v1.csv')
-salesData
-
-# 데이터 전처리
-salesData = refined_data(salesData)
-salesData
-
 import matplotlib.pyplot as plt
-
-# 이전 과제에서 작성한 그래프를 파이썬으로 구현
-
-totalSales = list(salesData['총매출'])
-unitPrices = list(salesData['1온스별단가'])
-salesVolumes = []
-
-for idx in range(len(totalSales)):
-    volume = round(totalSales[idx] / unitPrices[idx])
-    salesVolumes.append(volume)
-    
-advPrice = list(salesData['광고비용'])
-months = list(salesData.index)
-
-plt.style.use('default')
-plt.rcParams['font.size'] = 12
-
-fig, ax1 = plt.subplots()
-ax1.plot(months, salesVolumes, color='green')
-ax1.set_ylabel('salesVolumes')
-ax1.grid(True, axis = 'y')
-
-plt.ylim(2400000, 3200000)
-current_values = plt.gca().get_yticks()
-plt.gca().set_yticklabels(['$' + '{:,.0f}'.format(x) for x in current_values])
-
-ax2 = ax1.twinx()
-ax2.set_ylabel('advPrice')
-ax2.plot(months, advPrice, color='deeppink')
-
-plt.ylim(0, 1200000)
-current_values = plt.gca().get_yticks()
-plt.gca().set_yticklabels(['$' + '{:,.0f}'.format(x) for x in current_values])
-
-plt.show()
-
 
 # 데이터 불러오기
 salesData = load_data('A회사_보습제_매출데이터_v1_edited.csv')
